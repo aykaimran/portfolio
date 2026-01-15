e76de7be22d5e36929e2f8c069f2f1503a4c229b
aykaimran <aykaimran28@gmail.com>
2026-01-15 17:57:52 +0500

    [Improved Achievements]

--- src/components/AchievementsSection.jsx
+++ src/components/AchievementsSection.jsx
@@ -4,53 +4,62 @@ import { FaTrophy } from 'react-icons/fa';
 const achievementsData = [
     {
         id: 1,
+        date: "Fall 2024",
         title: "Gold Medalist (1st Position)",
         context: "Software Engineering Dept",
         desc: "Secured highest GPA in the department."
     },
     {
         id: 2,
+         date: "2019 - 2023",
         title: "Merit Scholarship",
-        context: "2019 – 2023",
+        context: "KIMS College",
         desc: "Awarded for consistent academic excellence."
     },
     {
         id: 3,
+        date: "Spring 2024, Fall 2025",
         title: "Dean’s Honor List",
         context: "FAST-NUCES",
         desc: "Recognized in Spring 2024 and Fall 2025."
     },
     {
         id: 4,
+         date: "2018 - 2023",
         title: "High Achiever Awards",
         context: "KIMS College",
         desc: "Secured five High Achiever Awards (2018, 2019, 2021, 2022, 2023) in 8th grade, o and a levels."
     },
+     {
+        id: 8,
+        date: "Aug 2024",
+        title: "Graphic Designing & UI/UX Course",
+        context: "NS Training (SMC-PRIVATE) Limited",
+        desc: "Completed professional training in graphic design and UI/UX."
+
+    },
     {
         id: 5,
+         date: "Oct 2025",
         title: "CodeBees Certification",
         context: "Competitive Programming",
-        desc: "Java Programming Certification (Oct 2025)."
+        desc: "Java Programming Certification"
     },
     {
         id: 6,
+         date: "2024 - 2025",
         title: "Co-Head of Decor",
         context: "IEEE RAS",
         desc: "Led society initiatives and organized major events."
     },
     {
         id: 7,
+        date: "2023",
         title: "Head of Security",
         context: "Sports Fest 2023",
         desc: "First female appointee to lead security operations."
     },
-    {
-        id: 8,
-        title: "Graphic Designing & UI/UX Course",
-        context: "NS Training (SMC-PRIVATE) Limited",
-        desc: "Completed professional training in graphic design and UI/UX."
-
-    }
+   
 ];
 
 function AchievementsSection() {
